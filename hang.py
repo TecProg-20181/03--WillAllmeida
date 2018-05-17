0e1722e3f067f89369708a0f7e413bb35893c609
William Almeida <will.allmeida@gmail.com>
2018-05-17 12:45:45 -0300

    Refactoring Guess class

--- hang.py
+++ hang.py
@@ -3,6 +3,44 @@ import string
 
 WORDLIST_FILENAME = "palavras.txt"
 
+class Guess:
+
+    def __init__(self):
+        self.guesses = 8
+
+    def isWordGuessed(self, secretWord, lettersGuessed):
+
+        for letter in secretWord:
+            if letter in lettersGuessed:
+                pass
+            else:
+                return False
+
+        return True
+
+    def getGuessedWord(self):
+
+         guessed = ''
+
+         return guessed
+
+    def isGuessedWord(self, letter, lettersGuessed, guessed):
+        for letter in secretWord:
+            if letter in lettersGuessed:
+                guessed += letter
+            else:
+                guessed += '_'
+
+        return guessed
+
+    def getGuesses(self):
+
+        return self.guesses
+
+    def decreaseGuesses(self):
+        self.guesses = self.getGuesses() - 1
+
+
 def loadWords():
 
     print "Loading word list from file..."
@@ -16,37 +54,11 @@ def loadWords():
     return random.choice(wordList)
 
 
-def isWordGuessed(secretWord, lettersGuessed):
-
-    for letter in secretWord:
-        if letter in lettersGuessed:
-            pass
-        else:
-            return False
-
-    return True
-
-def getGuessedWord():
-
-     guessed = ''
-
-     return guessed
-
 def getAvailableLetters():
-    import string
-    # 'abcdefghijklmnopqrstuvwxyz'
     available = string.ascii_lowercase
 
     return available
 
-def isGuessedWord(letter, lettersGuessed, guessed):
-    for letter in secretWord:
-        if letter in lettersGuessed:
-            guessed += letter
-        else:
-            guessed += '_'
-
-    return guessed
 
 def showWelcomeMessage(secretWord):
 
@@ -63,18 +75,18 @@ def manageAvailableLetters(available, lettersGuessed):
     print '------------'
 
 def hangman(secretWord):
+    guess = Guess()
 
-    guesses = 8
     lettersGuessed = []
 
     showWelcomeMessage(secretWord)
 
 
-    while  isWordGuessed(secretWord, lettersGuessed) == False and guesses >0:
+    while guess.isWordGuessed(secretWord, lettersGuessed) == False and guess.getGuesses() >0:
 
-        print 'You have ', guesses, 'guesses left.'
+        print 'You have ', guess.getGuesses() , 'guesses left.'
 
-        guessed = getGuessedWord()
+        guessed = guess.getGuessedWord()
 
         available = getAvailableLetters()
         manageAvailableLetters(available, lettersGuessed)
@@ -84,28 +96,27 @@ def hangman(secretWord):
 
         if letter in lettersGuessed:
 
-            guessed = isGuessedWord(letter, lettersGuessed, guessed)
+            guessed = guess.isGuessedWord(letter, lettersGuessed, guessed)
             print 'Oops! You have already guessed that letter: ', guessed
 
         elif letter in secretWord:
 
             lettersGuessed.append(letter)
 
-            guessed = isGuessedWord(letter, lettersGuessed, guessed)
+            guessed = guess.isGuessedWord(letter, lettersGuessed, guessed)
             print 'Good Guess: ', guessed
 
         else:
-
-            guesses -=1
+            guess.decreaseGuesses()
             lettersGuessed.append(letter)
 
-            guessed = isGuessedWord(letter, lettersGuessed, guessed)
+            guessed = guess.isGuessedWord(letter, lettersGuessed, guessed)
             print 'Oops! That letter is not in my word: ',  guessed
             print '------------'
 
     else:
 
-        if isWordGuessed(secretWord, lettersGuessed) == True:
+        if guess.isWordGuessed(secretWord, lettersGuessed) == True:
             print 'Congratulations, you won!'
 
         else:
